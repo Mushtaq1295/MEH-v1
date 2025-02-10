e4eb315910b068da2a0dc4e586649c217b6fbd7d
Mushtaq1295 <mohammadmushtaq1295@gmail.com>
2025-02-10 14:41:35 +0530

    deployed react command done

--- frontend/src/components/Engines/EngineCategories.jsx
+++ frontend/src/components/Engines/EngineCategories.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useEngines } from "../../contexts/EnginesContext";
+import { motion } from "framer-motion";
 
 const categories = ["ASHOK LEYLAND", "TATA", "BHARAT BENZ", "EICHER"];
 
@@ -15,14 +16,17 @@ const EngineCategories = () => {
 
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4">
-      {categories.map((category) => (
-        <div 
-          key={category} 
+      {categories.map((category, index) => (
+        <motion.div
+          key={category}
+          initial={{ x: index % 2 === 0 ? -200 : 200, opacity: 0 }} // Left for even, right for odd
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
           className="p-6 bg-gray-300 rounded-lg shadow-md text-center cursor-pointer hover:bg-gray-200 transition"
           onClick={() => handleCategorySelect(category)}
         >
           <h2 className="text-xl p-2 font-semibold">{category}</h2>
-        </div>
+        </motion.div>
       ))}
     </div>
   );
