c89a43c6fe06f36e517327fe8d3764f0c0391d2c
Mushtaq1295 <mohammadmushtaq1295@gmail.com>
2025-02-15 08:21:09 +0530

    Navbar modified

--- frontend/src/components/Header/Navbar.jsx
+++ frontend/src/components/Header/Navbar.jsx
@@ -65,7 +65,7 @@ const Navbar = () => {
             isMenuOpen ? "block" : "hidden"
           } w-full md:flex md:w-auto md:order-1`}
         >
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-zinc-900">
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900">
             {/* Home Link */}
             <li>
               <NavLink
