a7bfec377fbea02f0dc516949cc7749e0d2ac2f3
Mushtaq1295 <mohammadmushtaq1295@gmail.com>
2025-03-07 19:01:33 +0530

    my local changes

--- frontend/src/components/History/HistoryAllCards.jsx
+++ frontend/src/components/History/HistoryAllCards.jsx
@@ -4,35 +4,20 @@ import HistoryAccessories from "./HistoryAccessories/HistoryAccessories";
 import { Navigate, NavLink } from "react-router-dom";
 import { useHistoryAccessories } from "../../contexts/HistoryAccessoriesContext";
 
-
-
-
 const HistoryAllCards = () => {
-//   const context = useHistoryAccessories();
+  // const { HistoryAccessories } = useHistoryAccessories(); // Get data from context
 
-// if (!context) {
-//   console.error("Error: HistoryAccessoriesContext is missing. Make sure it's wrapped in HistoryAccessoriesProvider.");
-//   return null; // Prevent rendering if context is missing
-// }
-
-// const { HistoryAccessories } = context;
-
-
-
-  
-  // let totalValue = () => {
-  //   if (!HistoryAccessories || HistoryAccessories.length === 0) return 0;
-  
-  //   return HistoryAccessories.reduce((total, item) => {
-  //     return total + (item.price || 0); // Ensure `price` is valid
-  //   }, 0);
-  // };
-  let total = 1000000;
+  // // Calculate total price of all accessories
+  // const totalValue = HistoryAccessories.reduce((total, item) => {
+  //   return total + (item.price || 0); // Ensure price is valid
+  // }, 0);
   
   return (
     <>
     <div className="flex flex-row justify-between items-center m-4">
-      <h4 className="mt-1 text-2xl font-bold  text-white">Total: {total.toLocaleString('en-IN')} </h4>
+      <h4 className="mt-1 text-2xl font-bold  text-white">Total: 
+        {/* {total.toLocaleString('en-IN')}  */}
+        </h4>
       <NavLink
         className="cursor-pointer text-3xl text-white fa-solid fa-calendar"
         to="/history/datefilter"
@@ -40,7 +25,9 @@ const HistoryAllCards = () => {
       </div>
       <div className="flex flex-row justify-between items-center m-5">
       <h1 className="text-3xl font-bold  text-white">Engines </h1>  
-      <h4 className="text-[14px] font-bold  text-white">Total: {total.toLocaleString('en-IN')} </h4>
+      <h4 className="text-[14px] font-bold  text-white">Total: 
+        {/* {total.toLocaleString('en-IN')} */}
+         </h4>
       </div>
       
      
@@ -48,7 +35,9 @@ const HistoryAllCards = () => {
         
         <div className="flex flex-row justify-between items-center m-4">
       <h1 className="text-3xl font-bold  text-white">Accessories </h1>  
-      <h4 className="text-[14px] font-bold  text-white">Total: {total.toLocaleString('en-IN')} </h4>
+      <h4 className="text-[14px] font-bold  text-white">Total: 
+        {/* {total.toLocaleString('en-IN')} */}
+         </h4>
       </div>
       <HistoryAccessories/>
 
